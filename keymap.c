f37dbec010bf544eb6fd33acdd8487357a241298
benskia <benski.abraham@gmail.com>
2024-05-21 16:53:55 -0500

    update keymap

--- keymap.c
+++ keymap.c
@@ -9,12 +9,14 @@ enum layer_number {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+/*_______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,*/
+
 /* QWERTY
  */
 
 [_QWERTY] = LAYOUT(
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_UNDS,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,  KC_CAPS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,                                                   KC_ENT, MO(_RAISE), KC_BSPC, KC_RALT
@@ -23,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_LOWER] = LAYOUT(
-  _______, KC_LBRC, KC_LCBR, KC_LPRN, KC_LABK, KC_TILD,                   KC_PIPE, KC_RABK, KC_RPRN, KC_RCBR, KC_RBRC, _______,
+  _______, KC_LBRC, KC_LCBR, KC_LPRN, KC_LABK, _______,                   _______, KC_RABK, KC_RPRN, KC_RCBR, KC_RBRC, _______,
   _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DQUO,                   KC_DQUO, _______, _______, _______, _______, _______,
   _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_QUOT,                   KC_QUOT, _______, _______, _______, _______, KC_GRV,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______, _______, _______, _______, _______, KC_BSLS, _______,
@@ -33,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT(
-  _______, KC_LBRC, KC_LCBR, KC_LPRN, KC_LABK, KC_TILD,                   KC_PIPE, KC_RABK, KC_RPRN, KC_RCBR, KC_RBRC, _______,
-  _______, _______, _______, _______, _______, KC_DQUO,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
-  _______, KC_PLUS, KC_MINS, KC_ASTR, KC_SLSH, KC_QUOT,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_GRV,
+  _______, KC_PLUS, KC_MINS, KC_ASTR, KC_SLSH, KC_EXLM,                   _______, _______, _______, _______, _______, _______,
+  _______, KC_AT,   KC_PIPE, KC_AMPR, KC_BSLS, KC_TILD,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
+  _______, _______, _______, _______, _______, KC_UNDS,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_GRV,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSLS, _______,
   _______, _______, _______, _______,                                                       _______, _______, _______, _______
 ),
