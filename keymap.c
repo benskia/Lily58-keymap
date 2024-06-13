c4e81ed2d30a5be2ac62ef9c245a77d67eb4b3b6
benskia <benski.abraham@gmail.com>
2024-06-13 09:40:42 -0500

    split paren row across layers; swap arrow/pg navigation to opposite layer; add angle bracket/pointer/operator combines to bottom row

--- keymap.c
+++ keymap.c
@@ -26,10 +26,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_LOWER] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                   KC_LABK, KC_MINS, KC_EQL,  KC_RABK, KC_PLUS, _______,
-  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,                   KC_D,    KC_LBRC, KC_RBRC, KC_COMM, _______, _______,
-  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,                   KC_LPRN, KC_LCBR, KC_RCBR, KC_RPRN, _______, _______,
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______, _______, _______, _______, _______, KC_BSLS, _______,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  _______, _______, _______, _______, _______, _______,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
+  KC_ESC,  _______, KC_LBRC, KC_LCBR, KC_LPRN, KC_QUOT,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+  _______, KC_ASTR, KC_LABK, KC_MINS, KC_EQL,  KC_PLUS, _______, _______, _______, _______, _______, _______, KC_BSLS, _______,
   _______, _______, _______, _______,                                                       _______, _______, _______, _______
 ),
 
@@ -37,10 +37,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
     [_RAISE] = LAYOUT(
-  _______, KC_ASTR, KC_SLSH, KC_PLUS, KC_MINS, KC_EQL,                    _______, _______, _______, _______, _______, _______,
-  _______, _______, KC_CIRC, KC_HASH, KC_ASTR, KC_DLR,                    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
-  _______, _______, KC_LPRN, KC_LCBR, KC_RCBR, KC_RPRN,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_COLN, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  _______, _______, KC_CIRC, KC_HASH, KC_ASTR, KC_DLR,                    _______, _______, _______, _______, _______, _______,
+  _______, _______, KC_LPRN, KC_RCBR, KC_LCBR, KC_RPRN,                   KC_QUOT, KC_RPRN, KC_RCBR, KC_RBRC, KC_COLN, KC_ESC,
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_PLUS, KC_EQL,  KC_MINS, KC_RABK, _______, _______,
   _______, _______, _______, _______,                                                       _______, _______, _______, _______
 ),
 
